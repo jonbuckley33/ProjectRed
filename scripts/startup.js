8741fb5cbfaeaa4ee09733a684c8795e69f4fb05
Jon Buckley <jonbuckley33@gmail.com>
2014-04-25 01:14:31 -0400

    little bit of test structure

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,18 +1,23 @@
 function CanvasManager()
 {
+    //reference to stage object
+    var stage;
+
     this.init = function(canvasID)
     {
         //Create a stage by getting a reference to the canvas
-        stage = new createjs.Stage(canvasID);
+        this.stage = new createjs.Stage(canvasID);
         //Create a Shape DisplayObject.
         circle = new createjs.Shape();
         circle.graphics.beginFill("red").drawCircle(0, 0, 40);
         //Set position of Shape instance.
         circle.x = circle.y = 50;
         //Add Shape instance to stage display list.
-        stage.addChild(circle);
+        this.stage.addChild(circle);
         //Update stage will render next frame
-        stage.update();
+        this.stage.update();
     }
+
+
 }
 
