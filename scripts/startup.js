301029d1041395aef7616b2b498642f6490ce17b
LBPeraza <lukasperaza@gmail.com>
2014-05-01 01:27:21 -0400

    Second GameManager, new image, updated CanvasManager

--- scripts/startup.js
+++ scripts/startup.js
@@ -7,17 +7,14 @@ function CanvasManager()
     {
         //Create a stage by getting a reference to the canvas
         this.stage = new createjs.Stage(canvasID);
-        //Create a Shape DisplayObject.
-        circle = new createjs.Shape();
-        circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-        //Set position of Shape instance.
-        circle.x = circle.y = 50;
-        //Add Shape instance to stage display list.
-        this.stage.addChild(circle);
         //Update stage will render next frame
         this.stage.update();
     }
 
+    this.addChild = function(child) {
+        this.stage.addChild(child);
+    }
 
+    this.update = function() { this.stage.update(); }
 }
 
