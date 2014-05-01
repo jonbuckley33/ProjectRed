42f92c944a7b545a6e9e13d17146e89343354764
Jon Buckley <jonbuckley33@gmail.com>
2014-05-01 00:58:41 -0400

    cleaned up some of GM. got functioning game loop

--- scripts/GameManager.js
+++ scripts/GameManager.js
@@ -12,63 +12,52 @@ function GameManager()
 	var state = gameState.INITIALIZING;
 	var canvasManager; 
 	var world;
+	var circle;
 
 	var actors = [];
 	var bodies = [];
 
-	var createBird = function(skin) {
-		var birdFixture = new b2FixtureDef;
-		birdFixture.density = 1;
-		birdFixture.restitution = 0.6;
-		birdFixture.shape = new b2CircleShape(24 / SCALE);   // half of bird.png width divided by world scale for right size
-		var birdBodyDef = new b2BodyDef;
-		birdBodyDef.type = b2Body.b2_dynamicBody;
-		birdBodyDef.position.x = skin.x / SCALE;  // divide skin x and y by box2d scale to get right position
-		birdBodyDef.position.y = skin.y / SCALE;
-		var bird = world.CreateBody(birdBodyDef);
-		bird.CreateFixture(birdFixture);
-		bodies.push(bird);
-	}
-
-	this.init = function(canvasManager, world)
+	this.init = function(cm)
 	{
-		this.canvasManager = canvasManager;
-		this.world = world;
-
-		console.log("canvas manager", canvasManager)
+		canvasManager = cm;
+		
+		// Define the world
+		var gravity = new b2Vec2(0, -10);
+		var doSleep = true;
+			
+		world = new b2World(gravity, doSleep);
 
 		//var birdBMP = new createjs.Bitmap("public/images/bird.png");
-
-		var circle = new createjs.Shape();
-	    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-	    //Set position of Shape instance.
-	    circle.x = circle.y = 50;
-
-		this.canvasManager.stage.addChild(circle);
-		this.canvasManager.stage.update();
-		//createBird(birdBMP);
-
-		//var actor = new Actor(circleBody);
-
 
 		createjs.Ticker.addEventListener("tick", this.run);
 	};
 
+	//60 fps
 	var timeStep = 1.0/60;
 	var iteration = 1;
 
+	//state machine of game
 	this.run = function()
 	{
 		switch (state)
 		{
 			case gameState.INITIALIZING:
-				this.state = gameState.RUNNING;
+				state = gameState.RUNNING;
 				break;
 
 			case gameState.RUNNING:
-				this.world.Step(this.timeStep, iteration);
+				//step world
+				world.Step(this.timeStep, iteration);
 
-				this.canvasManager.stage.update();
+				//update positions of actors
+				for (var i = 0; i < actors.length; i++)
+				{
+					//gets appropriate placement in canvas
+					actor[i].update();
+				}
+
+				//repain
+				canvasManager.stage.update();
 				break;
 
 			case gameState.PAUSED: 
