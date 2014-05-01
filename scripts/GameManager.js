53b79316c9d7b3031eceab04b218327eb8ce7dec
Jon Buckley <jonbuckley33@gmail.com>
2014-05-01 00:06:42 -0400

    folders and things

--- scripts/GameManager.js
+++ scripts/GameManager.js
@@ -11,14 +11,51 @@ function GameManager()
 	//set the state initially to INITIALIZING
 	var state = gameState.INITIALIZING;
 	var canvasManager; 
+	var world;
 
 	var actors = [];
+	var bodies = [];
 
-	this.init = function(canvasManager)
+	var createBird = function(skin) {
+		var birdFixture = new b2FixtureDef;
+		birdFixture.density = 1;
+		birdFixture.restitution = 0.6;
+		birdFixture.shape = new b2CircleShape(24 / SCALE);   // half of bird.png width divided by world scale for right size
+		var birdBodyDef = new b2BodyDef;
+		birdBodyDef.type = b2Body.b2_dynamicBody;
+		birdBodyDef.position.x = skin.x / SCALE;  // divide skin x and y by box2d scale to get right position
+		birdBodyDef.position.y = skin.y / SCALE;
+		var bird = world.CreateBody(birdBodyDef);
+		bird.CreateFixture(birdFixture);
+		bodies.push(bird);
+	}
+
+	this.init = function(canvasManager, world)
 	{
 		this.canvasManager = canvasManager;
+		this.world = world;
+
+		console.log("canvas manager", canvasManager)
+
+		//var birdBMP = new createjs.Bitmap("public/images/bird.png");
+
+		var circle = new createjs.Shape();
+	    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+	    //Set position of Shape instance.
+	    circle.x = circle.y = 50;
+
+		this.canvasManager.stage.addChild(circle);
+		this.canvasManager.stage.update();
+		//createBird(birdBMP);
+
+		//var actor = new Actor(circleBody);
+
+
 		createjs.Ticker.addEventListener("tick", this.run);
 	};
+
+	var timeStep = 1.0/60;
+	var iteration = 1;
 
 	this.run = function()
 	{
@@ -29,6 +66,9 @@ function GameManager()
 				break;
 
 			case gameState.RUNNING:
+				this.world.Step(this.timeStep, iteration);
+
+				this.canvasManager.stage.update();
 				break;
 
 			case gameState.PAUSED: 
