8a3ca5548061f0b548592167ab3611fc51e79d0b
LBPeraza <lukasperaza@gmail.com>
2014-05-28 00:24:28 -0400

    loadbar fix

--- public/scripts/GameManager.js
+++ public/scripts/GameManager.js
@@ -21,25 +21,31 @@ function GameManager()
 	}
 
 	function createLoadingBar() {
-		loadBg = new createjs.Bitmap("public/images/load_bg.jpg");
-		loadBg.x = canvasManager.getCanvasWidth / 2;
-		loadBg.y = canvasManager.getCanvasHeight / 2;
+		var width = 125;
+		var height = 30;
+		loadBg = new createjs.Bitmap("images/load_bg.jpg");
+		loadBg.x = canvasManager.getCanvasWidth() / 2;
+		loadBg.y = canvasManager.getCanvasHeight() / 2;
+		loadBg.regX = width/2;
+		loadBg.regY = height/2;
 		canvasManager.stage.addChild(loadBg);
 
 		var loadSheet = new createjs.SpriteSheet({
-			images: ["public/images/loadbar.png"],
-			frames: {width: 125, height: 30, regX: 62.5, regY: 0},
+			images: ["images/loadbar.png"],
+			frames: {width: width, height: height,
+					 regX: 0, regY: 0},
 			animations: {
 				load: [0, 16, "load"]
 			}
 		})
 		loadBar = new createjs.Sprite(loadSheet, "load");
-		loadBar.x = loadBg.x;
-		loadBar.y = loadBg.y;
+		loadBar.x = loadBg.x - width/2;
+		loadBar.y = loadBg.y - height/2;
 		loadBar.gotoAndPlay("load");
 		canvasManager.stage.addChild(loadBar);
 
 		loadMask = new createjs.Shape();
+		loadMask.barSize = {width: width, height: height};
 
 		canvasManager.update();
 	}
@@ -50,12 +56,14 @@ function GameManager()
 
 	function progress(e) {
 		var percent = e.loaded;
-		loadMask.graphics.beginFill("black").drawRect(0, 0, percent*125, 30);
-		loadMask.cache(0, 0, 125, 30);
+		var width = loadMask.barSize.width;
+		var height = loadMask.barSize.height;
+		loadMask.graphics.beginFill("black").drawRect(0, 0, percent*width, height);
+		loadMask.cache(0, 0, width, height);
 		loadBar.filters = [
 			new createjs.AlphaMaskFilter(loadMask.cacheCanvas)
 		];
-		loadBar.cache(0, 0, 125, 30);
+		loadBar.cache(0, 0, width, height);
 		canvasManager.update();
 	}
 
